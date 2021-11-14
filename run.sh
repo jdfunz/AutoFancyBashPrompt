f70665e9978db706a2560beb68c8f5ebfd19297e
JDFunz <44926637+jdfunz@users.noreply.github.com>
2021-11-14 12:24:15 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,15 +5,12 @@ sleep 0.1
 echo "[FancyPrompt] Applying to .bashrc"
 echo "source $HOME/.fp.sh" >> $HOME/.bashrc
 
-function neofetch {
-    echo "[FancyPrompt] Applying neofetch"
-    curl -sSL https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch -o $HOME/.neofetch.sh
-    echo "bash $HOME/.neofetch.sh" >> $HOME/.bashrc
-    echo "[FancyPrompt] complete."
-}
-read -p "Install neofetch? (y/n)?" choice
-case "$choice" in 
-  y|Y ) echo "yes";;
-  n|N ) echo "[FancyPrompt] complete.";;
-  * ) echo "invalid";;
-esac
+read -p "Continue (y/n)?" CONT
+if [ "$CONT" = "y" ]; then
+  echo "[FancyPrompt] Applying neofetch"
+  curl -sSL https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch -o $HOME/.neofetch.sh
+  echo "bash $HOME/.neofetch.sh" >> $HOME/.bashrc
+  echo "[FancyPrompt] complete."
+else
+  echo "[FancyPrompt] complete."
+fi
