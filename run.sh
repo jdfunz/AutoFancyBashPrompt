d68377101d50f017ae53b4e56ed8bd969caaef3e
JDFunz <44926637+jdfunz@users.noreply.github.com>
2021-11-14 12:27:48 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,12 +5,7 @@ sleep 0.1
 echo "[FancyPrompt] Applying to .bashrc"
 echo "source $HOME/.fp.sh" >> $HOME/.bashrc
 
-read -p "Continue (y/n)?" CONT
-if [ "$CONT" = "y" ]; then
-  echo "[FancyPrompt] Applying neofetch"
-  curl -sSL https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch -o $HOME/.neofetch.sh
-  echo "bash $HOME/.neofetch.sh" >> $HOME/.bashrc
-  echo "[FancyPrompt] complete.";
-else
-  echo "[FancyPrompt] complete.";
-fi
+echo "[FancyPrompt] Applying neofetch"
+curl -sSL https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch -o $HOME/.neofetch.sh
+echo "bash $HOME/.neofetch.sh" >> $HOME/.bashrc
+echo "[FancyPrompt] complete.";
